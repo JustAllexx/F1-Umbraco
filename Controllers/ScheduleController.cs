88a1ccc28803e28fde874229ffcd7c34f38cc115
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-01-02 17:22:51 +0000

    Created Schedule API

---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using F1_site.Models;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace F1_site.Controllers
+{
+
+    [Route("api/schedule")]
+    public class ScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("getraces")]
+        public IEnumerable<CalendarRace> getRaces()
+        {
+            CalendarRace australia = new ("Australia", "FORMULA 1 AUSTRALIAN GRAND PRIX 2025", "MAR", "14-16");
+            CalendarRace dubai = new("China", "FORMULA 1 HEINEKEN GRAND PRIX 2025", "MAR", "21-23");
+            CalendarRace japan = new("Japan", "FORMULA 1 LENOVO JAPANESE GRAND PRIX 2025", "APR", "04-06");
+            return [australia, dubai, japan];
+        }
+    }
+}
---
+++ Models/CalendarRace.cs
@@ -0,0 +1,19 @@
+﻿namespace F1_site.Models
+{
+    public class CalendarRace
+    {
+
+        public CalendarRace(string v1, string v2, string v3, string v4)
+        {
+            place = v1;
+            name = v2;
+            month = v3;
+            days = v4;
+        }
+
+        public string place { get; set; }
+        public string name { get; set; }
+        public string month { get; set; }
+        public string days { get; set; }
+    }
+}

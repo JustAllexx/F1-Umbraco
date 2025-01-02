ec5cab236d0eee1679c6aa2b46c99e54cb8fcb55
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-01-02 18:54:17 +0000

    Added database for driver standings

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using F1_site.Models;
 using Umbraco.Cms.Web.Common.Controllers;
+using Microsoft.Data.Sqlite;
 
 namespace F1_site.Controllers
 {
@@ -20,6 +21,36 @@ namespace F1_site.Controllers
             CalendarRace dubai = new("China", "FORMULA 1 HEINEKEN GRAND PRIX 2025", "MAR", "21-23");
             CalendarRace japan = new("Japan", "FORMULA 1 LENOVO JAPANESE GRAND PRIX 2025", "APR", "04-06");
             return [australia, dubai, japan];
+        }
+
+        [HttpGet("standings")]
+        public IEnumerable<F1Driver> GetDrivers()
+        {
+            List<F1Driver> driverReturn = new();
+            string query = "SELECT DriverName, DriverTeam, DriverNumber, DriverPoints FROM Drivers ORDER BY DriverPoints DESC";
+            string resolvedPath = AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
+            using (var connection = new SqliteConnection($"Data Source={resolvedPath}\\Umbraco.sqlite.db"))
+            {
+                connection.Open();
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string driverName = reader.GetString(0);
+                            string driverTeam = reader.GetString(1);
+                            int driverNumber = reader.GetInt32(2);
+                            int driverPoints = reader.GetInt32(3);
+
+                            F1Driver a = new F1Driver(driverName, driverTeam, driverNumber, driverPoints);
+                            driverReturn.Add(a);
+                        }
+                    }
+                }
+            }
+
+            return driverReturn;
         }
     }
 }
---
+++ Models/F1Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace F1_site.Models
+{
+    public class F1Driver(string DriverName, string DriverTeam, int DriverNumber, int DriverPoints)
+    {
+        public string DriverName { get; set; } = DriverName;
+        public string DriverTeam { get; set; } = DriverTeam;
+        public int DriverNumber { get; set; } = DriverNumber;
+        public int DriverPoints { get; set; } = DriverPoints;
+    }
+}

f22286ffe34c714bae88cc3cb3644500d44cac21
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-01-05 18:21:09 +0000

    Added video to homepage

---
+++ wwwroot/js/video.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".video-button").on("click", function () {
+        $("body").css("overflow", "hidden");
+        $(this).closest(".video-container").find(".screen-cover").toggle();
+    });
+
+    $(".f1-close").on("click", function () {
+        $("body").css("overflow", "auto");
+        $(this).closest(".screen-cover").toggle();
+    });
+})
